19385c143018f9e24a477ade4cd9d0835ceeedd8
Lasha Mezvrishvili <mezzlasha@gmail.com>
2024-11-26 21:46:29 +0400

    give shimmer number of blocks so it looks better

--- lib/widgets/blocks/track_block_items_loader.dart
+++ lib/widgets/blocks/track_block_items_loader.dart
@@ -15,7 +15,7 @@ class TrackBlockItemsLoader<T> extends StatefulWidget {
 
   final Block block;
   final Widget Function(BuildContext context, List<T> items) builder;
-  final Widget Function(BuildContext context, BlockType type) loadingBuilder;
+  final Widget Function(BuildContext context, BlockType type, int length) loadingBuilder;
 
   @override
   State<TrackBlockItemsLoader<T>> createState() => _TrackBlockItemsLoaderState<T>();
@@ -38,7 +38,7 @@ class _TrackBlockItemsLoaderState<T> extends State<TrackBlockItemsLoader<T>> {
   Widget build(BuildContext context) => AnimatedSwitcher(
         duration: Durations.medium2,
         child: _loading
-            ? widget.loadingBuilder(context, widget.block.type)
+            ? widget.loadingBuilder(context, widget.block.type, _getLength())
             : widget.builder(context, _items),
       );
 
@@ -61,4 +61,11 @@ class _TrackBlockItemsLoaderState<T> extends State<TrackBlockItemsLoader<T>> {
       if (mounted) setState(() => _loading = false);
     }
   }
+
+  int _getLength() => switch (widget.block.type) {
+        BlockType.normal => (widget.block as NormalBlock).trackIds.length,
+        BlockType.featured => (widget.block as FeaturedBlock).trackIds.length,
+        BlockType.hero => 1,
+        BlockType.series => (widget.block as SeriesBlock).playlistIds.length,
+      };
 }
--- lib/widgets/blocks/track_block_loader.dart
+++ lib/widgets/blocks/track_block_loader.dart
@@ -22,12 +22,12 @@ class TrackBlockLoader extends StatelessWidget {
             block: block,
             builder: (_, items) =>
                 items.isNotEmpty ? HeroBlockItem(track: items.first) : Container(),
-            loadingBuilder: (_, type) => _shimmer(type),
+            loadingBuilder: _shimmer,
           ),
         BlockType.series => TrackBlockItemsLoader<AudioPlaylist>(
             block: block,
             builder: (_, items) => _buildSeriesBlock(items),
-            loadingBuilder: (_, type) => _shimmer(type),
+            loadingBuilder: _shimmer,
           ),
         BlockType.normal || BlockType.featured => TrackBlockItemsLoader<AudioTrack>(
             block: block,
@@ -35,7 +35,7 @@ class TrackBlockLoader extends StatelessWidget {
               items,
               block.type == BlockType.featured,
             ),
-            loadingBuilder: (_, type) => _shimmer(type),
+            loadingBuilder: _shimmer,
           ),
       };
 
@@ -80,7 +80,8 @@ class TrackBlockLoader extends StatelessWidget {
         ],
       );
 
-  static Widget _shimmer(BlockType type) => switch (type) {
+  static Widget _shimmer(BuildContext context, BlockType type, int length) =>
+      switch (type) {
         BlockType.hero => HeroBlockItem.shimmer(),
         BlockType.series => Column(
             // TODO: this may need start crossaxis
